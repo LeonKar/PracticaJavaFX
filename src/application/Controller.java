d757c4ba4f1ea975fe725f4aacb836d408439614
Leon <l.karagishev@gmail.com>
2016-11-18 10:58:25 +0100

    PracticaJavaFX

---
+++ src/application/Controller.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.io.IOException;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class Controller {
+	private Main main;
+	public void setMain(Main main) {
+        this.main = main;
+    }
+	
+	//not using yet
+	@FXML
+	private ImageView imageRaw;
+
+
+	/**
+     * Opens an stage to send the profile
+     */
+    
+    public void showSendProfile() {
+        try {
+            // Load the fxml file and create a new stage for the popup.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/Superheader.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            Stage sendStage = new Stage();
+            sendStage.setTitle("Superheader");
+            Scene scene = new Scene(page);
+             sendStage.setScene(scene);
+
+            
+            sendStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Method to handle the ImageView action in the Main stage 
+     */
+    
+    @FXML
+    private void handleSend() {
+    	showSendProfile();
+    }
+
+    /**
+     * Closes the application when clicked on Exit in the secondary stage
+     */
+    @FXML
+    private void handleClose() {
+   	        System.exit(0);
+    }
+    }
---
+++ src/application/Main.java
@@ -0,0 +1,79 @@
+package application;
+
+import java.io.IOException;
+
+import application.Controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+
+
+public class Main extends Application {
+	
+	private AnchorPane rootLayout;	
+	/**
+	 * my Stage is public
+	 */
+	public Stage primaryStage;
+
+	
+	/**
+	 * sets the stage
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			this.primaryStage = primaryStage;
+			this.primaryStage.setTitle("Practica JavaFX");
+			this.primaryStage.setX(200);
+			this.primaryStage.setY(200);
+
+			primaryStage.show();
+			
+			initRootLayout();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * loads the layout
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/Layouts.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout, 640, 480);
+			scene.getStylesheets().add(
+					getClass().getResource("micss.css").toExternalForm());
+	
+			//			adding fonts
+			scene.getStylesheets().add("http://fonts.googleapis.com/css?family=Shadows+Into+Light");
+            //Font.loadFont(getClass().getResourceAsStream("../resources/fonts/HipsterishFontNormal.ttf"), 20);
+            
+			primaryStage.setScene(scene);
+			
+			Controller controller = loader.getController();
+            controller.setMain(this);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args launching 
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
